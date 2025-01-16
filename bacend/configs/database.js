f7f37e008689a5753a2e2e4e2894c3e7548917d2
ah2727 <alihd7@proton.me>
2025-01-16 17:18:25 +0330

    checkpoint

--- bacend/configs/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-
-const sequelize = new Sequelize('hotel', process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: "mysql"
-});
