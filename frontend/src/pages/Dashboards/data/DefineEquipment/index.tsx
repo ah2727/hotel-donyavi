e19ee4873b00642ab05ba106339ab81c5b42df6d
ah2727 <alihd7@proton.me>
2025-01-17 18:28:21 +0330

    prototype for equipment define

--- frontend/src/pages/Dashboards/data/DefineEquipment/index.tsx
+++ frontend/src/pages/Dashboards/data/DefineEquipment/index.tsx
@@ -1,5 +1,56 @@
+import React from "react";
 const DefinEquipment = () => {
-
-    return(<div></div>)
-}
+  return (
+    <React.Fragment>
+      <div className="card mt-10">
+        <div className="card-body">
+          <form>
+            <div className="mb-4 flex flex-col items-center">
+              <div className="flex flex-col items-start ">
+                <label className="inline-block mb-2 text-base font-medium">
+                  نام دستگاه:
+                </label>
+                <input className="form-input px-[10vh] border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"></input>
+              </div>
+            </div>
+            <div className="mb-4 flex flex-col items-center">
+              <div className="flex flex-col items-start ">
+                <label className="inline-block mb-2 text-base font-medium">
+                  نوع تجهیزات:
+                </label>
+                <input className="form-input px-[10vh] border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"></input>
+              </div>
+            </div>
+            <div className="mb-4 flex flex-col items-center">
+              <div className="flex flex-col items-start ">
+                <label className="inline-block mb-2 text-base font-medium">
+                  نوع دستگاه:
+                </label>
+                <input className="form-input px-[10vh] border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"></input>
+              </div>
+            </div>
+            <div className="mb-4 flex flex-col items-center">
+              <div className="flex flex-col items-start ">
+                <label className="inline-block mb-2 text-base font-medium">
+                  توضیحات:
+                </label>
+                <input className="form-input px-[10vh] border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"></input>
+              </div>
+            </div>
+            <div className="mb-4 flex flex-col items-center">
+              <div className="flex flex-col items-start ">
+                <button
+                  type="submit"
+                  className="text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20"
+                >
+                  تایید
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 export default DefinEquipment;
